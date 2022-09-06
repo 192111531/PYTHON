e1b3a2150d54fc043597b432fe329ae4a527d685
192111531 <112922214+192111531@users.noreply.github.com>
2022-09-06 13:00:55 +0530

    Add files via upload

---
+++ grade.py
@@ -0,0 +1,81 @@
+print("enter the marks obtined in 5 subjects:")
+m1=int(input("enter the mark1:"))
+m2=int(input("enter the mark2:"))
+m3=int(input("enter the mark3:"))
+m4=int(input("enter the mark4:"))
+m5=int(input("enter the mark5:"))
+total=m1+m2+m3+m4+m5
+print("the total maek is:",total)
+avg=total/5
+if avg>=91 and avg<=100:
+    print("the grade is A:")
+elif avg>=81 and avg<=90:
+    print("the grade is B:")
+elif avg>=71 and avg<=80:
+    print("the grade is c:")
+elif avg>=61 and avg<=70:
+    print("the grade is D:")
+elif avg>=51 and avg<=60:
+    print("the grade is E:")
+else:
+    print("the grade is F:")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
