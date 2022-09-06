9b7cd7507cd8156c8bbb8a16019122ec6cd32231
192111531 <112922214+192111531@users.noreply.github.com>
2022-09-06 13:30:20 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,5 @@
+number=int(input("enter the number for which youn want to print multiplication table:"))
+print("the multiplication table of:",number)
+for count in range(1,11):
+    print(number,'x',count,'=',number*count)
+ 
